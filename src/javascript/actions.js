aaa299ef7f38a8639e9f4b89c3d7ebdb0c005121
captainkcodes <idashaglenn@icloud.com>
2019-12-13 17:57:45 -0600

    trying out javascript implementation

---
+++ src/javascript/actions.js
@@ -0,0 +1,13 @@
+function moveToLogin(){
+    let btn = document.getElementById('logbtn');
+    while (true){
+    window.location.href="src/login.html";
+    }
+}
+
+function moveToReg(){
+    let btn2 = document.getElementById('regbtn');
+    while (true){
+        window.location.href="src/reg.html";
+    }
+}
